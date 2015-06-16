6a811666120d5a1e0c278b05cde48bd5c6e5d4bb
sloppylopez <slopez@estudioweb130.com>
2015-06-17 00:16:08 +0100

    refactor in 3 parts

---
+++ DONTCOMMITME.md
@@ -0,0 +1,11 @@
+digitalocean backendtoken = 2b6c8dcccf0129c4ace5dd67aa32cc8c3897d50a13d4503635aac507a4829564
+passphrase sloppylopezkey.ppk = thisismypassphraseforsloppylopezkey
+ 
+sloppylopez@docker:~$ ssh-keygen -t rsa
+
+server ip : 46.101.9.198
+root password: 11N3m3s1s11
+
+ssh-keygen -R 46.101.9.198
+
+https://www.digitalocean.com/community/tutorials/how-to-use-ssh-keys-with-digitalocean-droplets
--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -9,9 +9,9 @@ sudo gem install travis
 #Encrypt your code climate key for Travis build and add it automatically to your .travis.yml
 #remember explicitly declare this variables in
 #https://travis-ci.org/<YOUR_GIT_USER>/<YOUR_GIT_PROJECT_NAME>/settings/env_vars
-travis encrypt CODECLIMATE_REPO_TOKEN=c73a9cf87d3e9233c20179c3d2b496dd73d9f8ef5664ef8ebe5ebbd9765ef823 --add
-travis encrypt FIREBASE_USER=sloppylopez --add
-travis encrypt FIREBASE_PASSWORD=F1r3b4s3! --add
+travis encrypt CODECLIMATE_REPO_TOKEN=<YOUR_CODE_CLIMATE_TOKEN> --add
+travis encrypt FIREBASE_USER=<YOUR_FIREBASE_USER> --add
+travis encrypt FIREBASE_PASSWORD=<YOUR_FIREBASE_PASSWORD> --add
 
 #Fix for node first install http://stackoverflow.com/questions/21168141/can-not-install-packages-using-node-package-manager-in-ubuntu
 sudo ln -s /usr/bin/nodejs /usr/bin/node
