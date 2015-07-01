a13c2593706eea06be0d29bcc22e29d3b1e369db
sloppylopez <slopez@estudioweb130.com>
2015-07-01 14:04:42 +0100

    fixed bug in error messages after change

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -8,16 +8,6 @@
 angular.module('angularClientApp')
     .run(['$rootScope', '$location', 'firebaseFactory', 'FB_USER', 'ngToast', function ($rootScope, $location, firebaseFactory, FB_USER, ngToast) {
         $rootScope.fbRef = firebaseFactory.getFireBaseRef(FB_USER);//TODO ensure it's not calling the real firebase while testing
-        //ngToast.config({
-        //  theme: 'pure',
-        //  position: 'bottom',
-        //  duration: 3000,
-        //  type: 'info',
-        //  sticky: false
-        //});
-        //    var warning = ngToast.warning({
-        //        content: '<a href="#" class="">a message</a>'
-        //    });
         $rootScope.ngToast = ngToast;
         $rootScope.$on('$routeChangeStart', function (event, next) {
             if (next.loginRequired && typeof $rootScope.authData === 'undefined') {
